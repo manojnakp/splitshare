325153c56bb7e888422819d7a476e3a376d5cee3
Manojna <manojnakp2003@gmail.com>
2023-01-15 00:06:55 +0530

    feat: routes to paths outline

---
+++ pkg/path/participant.go
@@ -0,0 +1,15 @@
+package path
+
+import "github.com/labstack/echo/v4"
+
+func AllPtpnts(c *echo.Context) error {
+	return nil
+}
+
+func AddPtpnt(c *echo.Context) error {
+	return nil
+}
+
+func DelPtpnt(c *echo.Context) error {
+	return nil
+}
