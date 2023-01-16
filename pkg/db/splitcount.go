7188895d576ec7da2fff6359df43d8e372e8766e
manojnakp <89296644+manojnakp@users.noreply.github.com>
2023-01-16 16:53:51 +0530

    fix: db splitcount collection

--- pkg/db/splitcount.go
+++ pkg/db/splitcount.go
@@ -1,23 +1,72 @@
 package db
 
-import "github.com/manojnakp/splitshare/pkg/component"
+import (
+	"errors"
+	"github.com/manojnakp/splitshare/pkg/component"
+)
 
 var _ Collection = &SCollection{}
 
-type SCollection []component.SplitCount
+var (
+	ErrNotFound    = errors.New("record not found")
+	ErrInvalidType = errors.New("invalid splitcount")
+)
+
+type SCollection []*component.SplitCount
 
 func (c *SCollection) Insert(doc any) error {
+	s, ok := doc.(component.SplitCount)
+	if !ok {
+		return ErrInvalidType
+	}
+	*c = append(*c, &s)
 	return nil
 }
 
 func (c *SCollection) Find(id string) (any, error) {
-	return nil, nil
+	for _, v := range *c {
+		if v.Id == id {
+			return *v, nil
+		}
+	}
+	return nil, ErrNotFound
 }
 
 func (c *SCollection) Update(id string, doc any) error {
-	return nil
+	s, ok := doc.(component.SplitCount)
+	if !ok {
+		return ErrInvalidType
+	}
+	for i, v := range *c {
+		if v.Id == id {
+			c.validateUpdate(i, s)
+			return nil
+		}
+	}
+	return ErrNotFound
 }
 
 func (c *SCollection) Delete(id string) error {
-	return nil
+	n := len(*c)
+	for i, v := range *c {
+		if v.Id == id {
+			(*c)[i], (*c)[n-1] = (*c)[n-1], (*c)[i]
+			(*c)[n-1] = nil
+			*c = (*c)[0 : n-1]
+			return nil
+		}
+	}
+	return ErrNotFound
+}
+
+func (c *SCollection) validateUpdate(i int, s component.SplitCount) {
+	if s.Title != "" {
+		(*c)[i].Title = s.Title
+	}
+	if s.Desc != "" {
+		(*c)[i].Desc = s.Desc
+	}
+	if s.By != "" {
+		(*c)[i].By = s.By
+	}
 }
